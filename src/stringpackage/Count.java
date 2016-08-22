01f5b41a77ad1a28109e86ca566f2e126d473945
HarshitaYeole1 <yeoleharshita@gmail.com>
2016-08-22 11:39:39 +0530

    Count occurences of words in a string/file

--- src/stringpackage/Count.java
+++ src/stringpackage/Count.java
@@ -10,15 +10,42 @@ import java.util.Scanner;
  */
 public class Count {
 
+    private FileReader file;
+    private Map<String, Integer> m = new HashMap<>();
 
-    public void findWord(FileReader file, int numberOfOccurence) {
+    public Count(FileReader file, Map<String, Integer> m) {
+        this.file = file;
+        this.m = m;
+
+
+    }
+
+    public FileReader getFile() {
+        return file;
+    }
+
+    public void setFile(FileReader file) {
+        this.file = file;
+    }
+
+    public Map<String, Integer> getM() {
+        return m;
+    }
+
+    public void setM(Map<String, Integer> m) {
+        this.m = m;
+    }
+
+    public void findWord() {
 
         Scanner sc = new Scanner(file);
         sc.useDelimiter("\\s+|\\.|\\,|\\;|\\:|\\'|\\?|\\!");
-        Map<String, Integer> m = new HashMap<>();
+
 
         while (sc.hasNext()) {
             String str = sc.next();
+            str = str.toLowerCase();             //to ignore cases
+
             if (m.containsKey(str)) {          //if key already exists in map
                 Integer count = m.get(str);    //get its value
                 m.put(str, count + 1);         //increment value and store the key-value
@@ -26,11 +53,15 @@ public class Count {
                 m.put(str, 1);                 //if key does not exist add key-value to map
             }
 
-
         }
+
+
+    }
+
+    public void printMap(int numberOfOccurences) {
         System.out.println("KEY : VALUE");
         for (Map.Entry<String, Integer> entry : m.entrySet()) {
-            if (entry.getValue() >= numberOfOccurence) {
+            if (entry.getValue() >= numberOfOccurences) {
 
                 System.out.println(entry.getKey() + " : " + entry.getValue());
             }
--- src/stringpackage/StringCount.java
+++ src/stringpackage/StringCount.java
@@ -2,11 +2,14 @@ package stringpackage;
 /**
  * Created by harshita on 21/8/16.
  */
+
 import stringpackage.Count;
 
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -24,11 +27,14 @@ public class StringCount {
 
         //ask for occurence count to the user
         System.out.println("Enter the minimum number of occurence: ");
-        Scanner sc=new Scanner(System.in);
-        int numberOfOccurence=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int numberOfOccurence = sc.nextInt();
 
-        Count count=new Count();
-        count.findWord(file, numberOfOccurence);
+        Map<String, Integer> m = new HashMap<>();
+        Count count = new Count(file, m);
+
+        count.findWord();
+        count.printMap(numberOfOccurence);
         file.close();
 
     }
